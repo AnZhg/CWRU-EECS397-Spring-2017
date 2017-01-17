1f2ee0c0285ed3346cfcf600e22799d3361e94dd
安志钢 <an.zhigang@icloud.com>
2017-01-17 13:35:32 -0500

    Add sample code for assignment 1.

---
+++ Samples/Blink.c
@@ -0,0 +1,60 @@
+// Create by AnZhg on Jan. 14, 2017.
+// Case Western Reserve University
+//
+// Compile: gcc -Wall -Wextra -Werror -Os NAME.c -o NAME
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    
+    // Make these GPIO's available.
+    system("echo \"17\" > /sys/class/gpio/export");    // Red.
+    system("echo \"27\" > /sys/class/gpio/export");    // Green.
+    system("echo \"22\" > /sys/class/gpio/export");    // Blue.
+    
+    // Set direction.
+    system("echo out > /sys/class/gpio/gpio17/direction");
+    system("echo out > /sys/class/gpio/gpio27/direction");
+    system("echo out > /sys/class/gpio/gpio22/direction");
+    
+    // Red.
+    // Turn on.
+    system("echo \"1\" > /sys/class/gpio/gpio17/value");
+    
+    // Delay around 2 seconds.
+    system("sleep 2");
+    
+    // Turn off.
+    system("echo \"0\" > /sys/class/gpio/gpio17/value");
+    
+    // Green.
+    // Turn on.
+    system("echo \"1\" > /sys/class/gpio/gpio27/value");
+    
+    // Delay around 2 seconds.
+    system("sleep 2");
+    
+    // Turn off.
+    system("echo \"0\" > /sys/class/gpio/gpio27/value");
+    
+    // Blue.
+    // Turn on.
+    system("echo \"1\" > /sys/class/gpio/gpio22/value");
+    
+    // Delay around 2 seconds.
+    system("sleep 2");
+    
+    // Turn off.
+    system("echo \"0\" > /sys/class/gpio/gpio22/value");
+    
+    // Give GPIO resources back to operating system.
+    system("echo \"17\" > /sys/class/gpio/unexport");
+    system("echo \"27\" > /sys/class/gpio/unexport");
+    system("echo \"22\" > /sys/class/gpio/unexport");
+    
+    return 0;
+}
